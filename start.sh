852a65b41c4a9f25b9b9d1ab4c37f667f9ca2c0a
Jorge Navarro <jnavarro@okode.com>
2019-05-08 15:19:00 +0200

    Start finish script

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-API_KEY=################
-time=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-day_month=$(date -u +'%d-%m')
+API_KEY=XNKZBhCA7E18SKic
+TIME=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+DAY_MONTH=$(date -u +'%d-%m')
 
 curl -X POST \
   https://api.clockify.me/api/v1/workspaces/5cad884169b7cc19c5c2cae8/time-entries \
   -H "X-Api-Key: ${API_KEY}" \
   -H 'content-type: application/json' \
   -d '{
-    "start": "'${time}'",
+    "start": "'${TIME}'",
     "billable": true,
-    "description": "Inicio jornada '${day_month}'",
+    "description": "Inicio jornada '${DAY_MONTH}'",
     "projectId": "5cad987e1080ec1cfa267c5d"
   }'
